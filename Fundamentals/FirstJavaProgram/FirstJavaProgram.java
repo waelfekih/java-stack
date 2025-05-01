499de1f39bb54c9e1e789665b48d20eb17f32997
waelfekih <waaelfkh@gmail.com>
2025-05-01 10:58:43 +0200

    java

---
+++ Fundamentals/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main (String[] args) {
+        System.out.println("My name is wael");
+        System.out.println("I'm 29 years old");
+        System.out.println("My home town is Nabeul, Tunisia");
+    }
+}
