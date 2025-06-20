bdea819cf29fbc48ab887825d35155a3b1bd4ac2
waelfekih <waaelfkh@gmail.com>
2025-06-20 18:12:55 +0100

    Spring CRUD

---
+++ Spring Boot/booksAPI/src/main/java/com/booksAPI/main/repositories/BookRepository.java
@@ -0,0 +1,14 @@
+package com.booksAPI.main.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.booksAPI.main.models.Book;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
+	List<Book> findAll();
+	List<Book> findByDescriptionContaining(String search);
+}
